ed6b03b04b999b6c8591bf3f60b8f8916cfb96a9
Taslim <taslimarif600@gmail.com>
2021-07-24 20:40:15 +0530

    feature(master): Refactored fake file generator code

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 const fs = require("fs-extra")
-const path = require("path")
 const FakeFileGenerator = require('fake-file-generator');
-const filePath = 'output';
-const fileName = "hello.txt";
-if (!fs.existsSync(filePath)){
-    fs.mkdirSync(filePath);
-}
-const size = 10000000;
-const options = { type: 'txt' }
-FakeFileGenerator.makeFile(filePath+'/'+fileName, size, options)
-    .then(() => {
-      console.log('file generated!');
-      setTimeout(() => {
-        const removeDir = function(path) {
-  if (fs.existsSync(path)) {
-    const files = fs.readdirSync(path)
 
+/*
+ ***************************************** Generate Fake file and folder *******************************
+*/
+function GenerateFolderAndFakeFile(fileSize, fileType, filePath, fileName) {
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath);
+    console.log('Folder created');
+  }
+  const options = { type: fileType }
+  FakeFileGenerator.makeFile(filePath + '/' + fileName, fileSize, options);
+  console.log('file generated!');
+}
+
+function RemoveFileAndFolder(filePath) {
+  /*
+   ************************************** Remove File and Folder *******************************************                   
+  */
+  if (fs.existsSync(filePath)) {
+    const files = fs.readdirSync(filePath)
+    /*
+      ******************** If 1 or more folders/files exist inside directory ****************************
+    */
     if (files.length > 0) {
-      files.forEach(function(filename) {
-        if (fs.statSync(path + "/" + filename).isDirectory()) {
-          removeDir(path + "/" + filename)
-        } else {
-          fs.unlinkSync(path + "/" + filename)
+      files.forEach((filename)=> {
+        /*
+         ********************* If it is a directory ****************************
+        */
+        if (fs.statSync(filePath + "/" + filename).isDirectory()) {
+          /*
+           ******************* Recursive call until all the sub folders are deleted inside a folder ************************
+          */
+          RemoveFileAndFolder(filePath + "/" + filename)
+          console.log('Removed Folder');
+        }
+        /*
+         ********************* If it is a file ****************************
+        */
+        else {
+          fs.unlinkSync(filePath + "/" + filename)
+          console.log('Removed File');
         }
       })
-      fs.rmdirSync(path)
+      fs.rmdirSync(filePath);
+      console.log('Removed Folder');
     } else {
-      fs.rmdirSync(path)
+      fs.rmdirSync(filePath);
     }
-  } else {
+  }
+   /*
+      ******************** If no files/folder exist inside directory ****************************
+    */
+  else {
     console.log("Directory path not found.")
   }
 }
 
-removeDir(filePath)
-      },2000);
-    })
-    .catch(console.error);
+// GenerateFolderAndFakeFile(100000, 'txt', 'tas', 'test.txt');
+// RemoveFileAndFolder('tas');
